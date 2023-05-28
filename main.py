42708e80d8e851f5abc3edd8587118f935cdb03b
docker3 <MY_NAME@example.com>
2023-05-28 22:38:40 +0200

    Docker swarm implementation New Telegram API wrapper

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import public_ip as ip
 from tinydb import TinyDB, Query
 import telegram
-import time
+import time, os
 
-db = TinyDB('/tmp/ip_database.json') # TODO change DB folder to persistant one
+db = TinyDB('/tmp/ip_database.json')
 
 def get_public_ip():
     return ip.get()
@@ -28,15 +28,16 @@ def add_ip_to_database(ip_address):
     else:
         db.insert({'ip_address': ip_address, 'timestamp': timestamp})
         if ip_address != last_ip_address:
-            telegram.send_message(f"New host IP {ip_address}","") # TODO print hostname whose IP has changed
+            telegram.send_message(f"{os.uname()[1]} {ip_address}","") 
         return ip_address != last_ip_address
 
 def main():
     ip_address = get_public_ip()
     add_ip_to_database(ip_address)
-    print(f"{time.strftime('%Y-%m-%d %H:%M:%S')}: Current IP -> {ip_address}") # TODO print hostname whose IP has changed
-
+    print(f"{time.strftime('%Y-%m-%d %H:%M:%S')}: {os.uname()[1]} Current IP -> {ip_address}") 
 
 if __name__ == "__main__":
 
-    main()
+    while(True):
+        main()
+        time.sleep(60*2)
--- telegram.py
+++ telegram.py
@@ -1,21 +1,43 @@
-from telethon import TelegramClient, events, sync
-import os
+import requests
+from dotenv import dotenv_values
 
+config = dotenv_values("/run/secrets/telegram-secrets")
 
-api_id = os.environ.get('TELEGRAM_API_ID') 
-api_hash = os.environ.get('TELEGRAM_API_HASH')
-bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
-chat_id = os.environ.get('TELEGRAM_CHAT_ID')
-
-client = TelegramClient('myclient', api_id, api_hash).start(bot_token=bot_token)
-client.start()
+bot_token = config["TELEGRAM_BOT_TOKEN"]
+chat_id = config["TELEGRAM_CHAT_ID"]
 
 def send_message(message,url):
-    client.send_message(chat_id, '**'+message+'** '+url, link_preview=False)
+    req_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
 
-def send_image(thumb):
-    client.send_file(chat_id, thumb)
+    if len(url)>0:
+        url_formated = '['+url+']('+url+')'
+    else: url_formated = ''
+    params = {
+        'chat_id': chat_id,
+        'text': message + ' \n'+ url ,
+        'disable_web_page_preview': True,
+    }
+    response = requests.post(req_url, params=params)
+    if response.status_code == 200:
+        return True
+    else:
+        print(response.text)
+        return False
+
+def send_image(image):
+    req_url = f"https://api.telegram.org/bot{bot_token}/sendPhoto"
+    params = {
+        'chat_id': chat_id,
+        'photo': image
+    }
+    response = requests.post(req_url, params=params)
+    if response.status_code == 200:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
-    send_image("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/240px-Telegram_logo.svg.png")
+    print
+
+    #send_image("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/240px-Telegram_logo.svg.png")
     send_message("Telegram Logo","https://es.m.wikipedia.org/wiki/Archivo:Telegram_logo.svg")
